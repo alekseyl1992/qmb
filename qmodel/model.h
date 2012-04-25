df1ef737b300cc722ede8d294cf7198429506bdb
igorcoding <igorcoding@gmail.com>
2012-04-25 17:08:13 +0000

    поправил model

--- qmodel/model.h
+++ qmodel/model.h
@@ -21,68 +21,37 @@ namespace qmodel
 	//это входной вектор логики
 	//и выходной из Converter
 	
-    class model : public object
+	class model : public object
 	{
-		
-	public:
 		std::mutex model_mutex;
-        model(): object(++cur_id) { }
+	public:
+		model(): object(++cur_id) { }
 		model(const model& ) { }
 
-		~model()
-		{
-		}
+		~model() { }
 
-		std::vector<generator> generators;
-		std::vector<queue> queues;
-		std::vector<handler> handlers;
+		bool is_all_generated(); //checks if all generators finished their work
+		bool is_queues_clear(); //checks if all queues are clear
+		bool is_simulating(); //checks the sumulation
+
+		virtual void clean() { }
+
+		void simulation_start();
+		void simulation_stop();
+
+	private:
+		void generator_queue_link_th();
+		void handler_queue_link_th();
+
+	public: //members <- TODO: make private
+		std::vector<generator> generators; //all generators are kept here
+		std::vector<queue> queues; //all queues are kept here
+		std::vector<handler> handlers; //all handlers are kept here
 
 		//links
 		std::vector< link <generator*, queue*> > link_generators_queues;
 		std::vector< link <queue*, handler*> > link_queues_handlers;
 
-		bool is_all_generated() {
-			bool all_completed = true;
-			std::for_each(link_generators_queues.begin(), link_generators_queues.end(), [&all_completed](link <generator*, queue*>& link) {
-				if (!link.lhs->is_finished())
-				{
-					all_completed = false;
-				}
-			});
-			return all_completed;
-		}
-
-		bool is_queue_clear() {
-			bool all_completed = true;
-			std::for_each(link_queues_handlers.begin(), link_queues_handlers.end(), [&all_completed](link <queue*, handler*>& link) {
-				if (link.lhs->get_size() != 0)
-				{
-					all_completed = false;
-				}
-			});
-			return all_completed;
-		}
-
-		bool is_simulating() {
-			bool ag = is_all_generated();
-			bool qc = is_queue_clear();
-			return !(is_all_generated() && is_queue_clear());
-		}
-
-		//algorithm
-		public:
-			void simulation_start();
-			void simulation_stop();
-		private:
-			void generator_queue_link_th();
-			void handler_queue_link_th();
-
-			
-		std::vector<std::thread*> threads;
-
-		virtual void clean() { }
-
-	private:
 		bool simulate_flag;
 		static int cur_id;
 	};
