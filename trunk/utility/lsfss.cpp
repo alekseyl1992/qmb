511e6f38f27b297806710d423e0f5f6e06ad759f
igorcoding@gmail.com <igorcoding@gmail.com@e2413920-eb82-ff91-3ec2-928c907d78bc>
2012-05-02 17:57:54 +0000

    ничего особенного

--- trunk/utility/lsfss.cpp
+++ trunk/utility/lsfss.cpp
@@ -1,12 +1,5 @@
 #include "lsfss.h"
 
-/*static*/ bool connect(QObject *sender, const char *signal, const std::function<void()> &reciever, Qt::ConnectionType type) {
+bool connect(QObject *sender, const char *signal, const std::function<void()> &reciever, Qt::ConnectionType type) {
     return QObject::connect(sender, signal, new connect_functor_helper(sender, reciever), SLOT(signaled()), type);
 }
-
-/*
-static bool connect(QObject *sender, const char *signal, const std::function<void()> &reciever, Qt::ConnectionType type = Qt::AutoConnection) {
-    return QObject::connect(sender, signal, new connect_functor_helper(sender, reciever), SLOT(signaled()), type);
-}
-
-*/
