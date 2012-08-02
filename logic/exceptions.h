d557ec9f68165db0e5510272ac59245f7f9b3045
igorcoding <igorcoding@gmail.com>
2012-08-02 06:28:13 +0000

    добавлено исключение ManyInputsOutputsException несколько внутренних изменений в model

--- logic/exceptions.h
+++ logic/exceptions.h
@@ -3,9 +3,14 @@
 
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <stdlib.h>
+
+#include "object.h"
 
 namespace logic
 {
+
 	namespace exceptions
 	{
         class LogicException : public std::logic_error
@@ -17,6 +22,41 @@ namespace logic
             LogicException(const std::string& message)
                 : std::logic_error(message)
             { }
+        };
+
+        class IConstructableException
+        {
+        protected:
+            std::string intToString(int val)
+            {
+                int digitsCount = 0;
+                int tmp = val;
+                while(tmp > 0)
+                {
+                    tmp /= 10;
+                    ++digitsCount;
+                }
+
+                char* txt_num = new char[digitsCount + 1];
+                itoa(val, txt_num, 10);
+
+                std::string res(txt_num);
+
+                delete txt_num;
+                txt_num = nullptr;
+
+                return res;
+            }
+            std::string ConstructIDs(std::vector<object*>& bad_objects)
+            {
+                std::string msgIDs = "";
+                for (auto it = bad_objects.begin(); it != bad_objects.end() - 1; ++it)
+                {
+                    msgIDs += intToString((*it)->get_id()) + ", ";
+                }
+                msgIDs += intToString((*(bad_objects.end() - 1))->get_id());
+                return msgIDs;
+            }
         };
 
         class NoGeneratorsException : public LogicException
@@ -43,16 +83,23 @@ namespace logic
             { }
         };
 
-        class NoExitPointException : public LogicException
+        class NoExitPointException : public LogicException, public IConstructableException
         {
         public:
+            NoExitPointException(std::vector<object*>& bad_generators)
+                : LogicException(ConstructMessage(bad_generators))
+            { }
+
             NoExitPointException(const char* const & message)
                 : LogicException(message)
             { }
 
-            NoExitPointException(std::string message)
-                : LogicException(message.c_str())
-            { }
+            std::string ConstructMessage(std::vector<object*>& bad_generators)
+            {
+                std::string msg = "Не для всех генераторов существует выходная точка:\n[id]";
+                msg += ConstructIDs(bad_generators);
+                return msg;
+            }
         };
 
         class TooBigIDException : public LogicException
@@ -61,9 +108,35 @@ namespace logic
             TooBigIDException()
                 : LogicException("Превышен лимит для идентификатора запроса. Симуляция будет прервана")
             { }
+
             TooBigIDException(const char* const & message)
                 : LogicException(message)
             { }
+        };
+
+        class ManyInputsOutputsException : public LogicException, public IConstructableException
+        {
+        public:
+            ManyInputsOutputsException(std::vector<object*>& bad_inputs_objects, std::vector<object*>& bad_outputs_objects)
+                : LogicException(ConstructMessage(bad_inputs_objects, bad_outputs_objects))
+            { }
+
+            ManyInputsOutputsException(const char* const & message)
+                : LogicException(message)
+            { }
+
+        private:
+            std::string ConstructMessage(std::vector<object*>& bad_inputs_objects, std::vector<object*>& bad_outputs_objects)
+            {
+                std::string msg = "";
+                if (bad_inputs_objects.size() != 0)
+                    msg += "У объектов со следующими id существует не один вход (используйте Собиратель):\n[id]" + ConstructIDs(bad_inputs_objects);
+
+                if (bad_outputs_objects.size() != 0)
+                    msg += "\nУ объектов со следующими id существует не один выход (используйте Разделитель):\n[id]" + ConstructIDs(bad_outputs_objects);
+
+                return msg;
+            }
         };
 
         class JSSepOutputNotSpecified : public LogicException
